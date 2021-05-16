b5f79a9fbae42e840d3ab518c869ed2932982644
Javier Perez <javier21perez98@gmail.com>
2021-05-16 15:02:02 -0700

    added jsx for footer

---
+++ src/componets/footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+	return (
+		<footer class="d-flex flex-column py-3 bg-gray">
+			<h4 id="contact-me" class="text-danger text-center">
+				<i class="fas fa-chevron-left"></i>Contact Me
+				<i class="fas fa-chevron-right"></i>
+			</h4>
+		</footer>
+	);
+}
+
+export default Footer;
