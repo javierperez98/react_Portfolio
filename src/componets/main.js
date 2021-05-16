03bdfc0cba3533e715c965b7c602c88b2e46f116
Javier Perez <javier21perez98@gmail.com>
2021-05-16 16:09:17 -0700

    imported projects jsx file

--- src/componets/main.js
+++ src/componets/main.js
@@ -1,9 +1,11 @@
 import About from "./about";
+import Projects from "./projects";
 
 function Main() {
 	return (
 		<main className="bg-custom pb-3">
 			<About />
+			<Projects />
 		</main>
 	);
 }
