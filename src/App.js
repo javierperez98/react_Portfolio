7fd471ec1471b4db1212fa305e2806c3c2a3441f
Javier Perez <javier21perez98@gmail.com>
2021-05-16 15:00:26 -0700

    imported main tag jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./componets/navbar";
 import Home from "./componets/home";
+import Main from "./componets/main";
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Home />
+			<Main />
 		</>
 	);
 }
