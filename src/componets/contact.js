80125a2acab185c8756cb50c3629e28050b83d3c
Javier Perez <javier21perez98@gmail.com>
2021-05-16 15:01:38 -0700

    added jsx for contact me links

---
+++ src/componets/contact.js
@@ -0,0 +1,20 @@
+function Contact() {
+	return (
+		<div class="d-flex justify-content-around">
+			<a class="text-info" href="https://github.com/javierperez98">
+				<i class="fab fa-github fa-2x"></i>
+			</a>
+			<a class="text-success" href="mailto:javier21perez98@gmail.com">
+				<i class="far fa-envelope fa-2x"></i>
+			</a>
+			<a class="text-primary" href="https://www.linkedin.com/in/javier-perez98/">
+				<i class="fab fa-linkedin-in fa-2x"></i>
+			</a>
+			<span class="text-warning">
+				<i class="fa fa-copyright fa-2x"></i>
+			</span>
+		</div>
+	);
+}
+
+export default Contact;
