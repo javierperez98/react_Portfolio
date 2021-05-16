61286a79c4235d1b6f7731a9dcee4ba97d483426
Javier Perez <javier21perez98@gmail.com>
2021-05-16 13:32:13 -0700

    created a navLinks componet

---
+++ src/componets/navLinks.js
@@ -0,0 +1,23 @@
+function NavLinks() {
+	return (
+		<div className="navbar-nav">
+			<a className="nav-link text-white" href="#about-me">
+				About Me
+			</a>
+			<a className="nav-link text-white" href="#projects">
+				My Projects
+			</a>
+			<a className="nav-link text-white" href="#contact-me">
+				Contact Me
+			</a>
+			<a
+				className="nav-link text-white"
+				href="https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing"
+			>
+				My Resume
+			</a>
+		</div>
+	);
+}
+
+export default NavLinks;
