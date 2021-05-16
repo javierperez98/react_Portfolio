483fa34402a2b3f5743bcc3686fa7ab8c7190522
Javier Perez <javier21perez98@gmail.com>
2021-05-16 13:31:31 -0700

    created a home navlink componet

---
+++ src/componets/navHome.js
@@ -0,0 +1,10 @@
+function NavHome() {
+	return (
+		<h2 className="nav-link m-0">
+			<a className="nav-link p-0 text-white" href="#home">
+				Home
+			</a>
+		</h2>
+	);
+}
+export default NavHome;
