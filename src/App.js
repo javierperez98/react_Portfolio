d07b409c387a3fdc55a7947951c24b60beb7606b
Javier Perez <javier21perez98@gmail.com>
2021-05-17 00:57:07 -0700

    created route paths for homepage, about page, project page and resume page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./componets/navbar";
 import Home from "./componets/home";
 import Main from "./componets/main";
 import Footer from "./componets/footer";
+import Projects from "./componets/projects";
+import About from "./componets/about";
+import Resume from "./componets/resume";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Navbar />
-			<Home />
-			<Main />
+			<Route exact path="/">
+				<Home to="/" />
+			</Route>
+			<Main>
+				<Route exact path="/about">
+					<About />
+				</Route>
+				<Route exact path="/projects">
+					<Projects />
+				</Route>
+				<Route exact path="/resume">
+					<Resume />
+				</Route>
+			</Main>
 			<Footer />
-		</>
+		</Router>
 	);
 }
 
