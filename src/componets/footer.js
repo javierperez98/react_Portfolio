6613167c614d1c9cecd805ed76208272bea4433f
Javier Perez <javier21perez98@gmail.com>
2021-05-17 00:58:47 -0700

    updated footer text and layout

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -2,12 +2,22 @@ import Contacts from "./contact";
 
 function Footer() {
 	return (
-		<footer className="d-flex flex-column py-3 bg-gray">
-			<h4 id="contact-me" className="text-danger text-center">
+		<footer className="d-flex flex-column py-5 bg-gray align-items-center">
+			<h2 id="contact-me" className="text-danger text-center mb-4">
 				<i className="fas fa-chevron-left"></i>Contact Me
 				<i className="fas fa-chevron-right"></i>
-			</h4>
+			</h2>
 			<Contacts />
+			<h5 className="text-white text-center my-5">Javier Perez</h5>
+			<p className="text-secondary">
+				<i className="fa fa-copyright"></i> Copyright 2021
+			</p>
+			<p className="text-secondary text-center">
+				All rights reserved. Powered by{" "}
+				<span className="text-white">
+					React.js<i className="p-1 fab fa-react text-react"></i>
+				</span>
+			</p>
 		</footer>
 	);
 }
