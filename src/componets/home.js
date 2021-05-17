fd47a276c5811661f027d07eba8835741bd65d96
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:16:45 -0700

    fixed layout

--- src/componets/home.js
+++ src/componets/home.js
@@ -1,24 +1,17 @@
 function Home() {
 	return (
 		<header
-			className="
-			vh-100
-			bg-custom
-			d-flex
-			flex-column
-			align-items-center
-			justify-content-center
-		"
+			className="vh-100 bg-custom flex-column d-flex justify-content-center"
 			id="home"
 		>
-			<h1 className="text-warning my-3">
+			<h1 className="text-warning text-center my-3 mx-auto">
 				<i className="fas fa-chevron-left"></i>Hello!/
 				<i className="fas fa-chevron-right"></i>
 			</h1>
-			<h2 className="text-primary text-center my-3">
+			<h2 className="text-primary text-center my-3 mx-auto">
 				<i className="fas fa-code-branch"></i>I'm Full-Stack Web-Developer,
 			</h2>
-			<h2 className="text-danger text-center my-3">
+			<h2 className="text-danger text-center my-3 mx-auto">
 				<i className="fas fa-terminal"></i>Javier Perez
 			</h2>
 		</header>
