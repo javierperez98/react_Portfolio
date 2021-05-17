11e51111d5401e732b6d2348f59cc3de93a171ea
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:16:15 -0700

    fixed error with class name

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -2,10 +2,10 @@ import Contacts from "./contact";
 
 function Footer() {
 	return (
-		<footer class="d-flex flex-column py-3 bg-gray">
-			<h4 id="contact-me" class="text-danger text-center">
-				<i class="fas fa-chevron-left"></i>Contact Me
-				<i class="fas fa-chevron-right"></i>
+		<footer className="d-flex flex-column py-3 bg-gray">
+			<h4 id="contact-me" className="text-danger text-center">
+				<i className="fas fa-chevron-left"></i>Contact Me
+				<i className="fas fa-chevron-right"></i>
 			</h4>
 			<Contacts />
 		</footer>
