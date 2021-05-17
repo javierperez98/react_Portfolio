118c9e30759dbd854ea16d4a760b7e50814f5592
Javier Perez <javier21perez98@gmail.com>
2021-05-17 01:02:48 -0700

    updated navlink paths and hrefs

--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -1,12 +1,8 @@
 const arr = [
-	{ link: "#about-me", text: "About Me" },
-	{ link: "#projects", text: "My Projects" },
+	{ link: "/about", text: "About Me" },
+	{ link: "/projects", text: "My Projects" },
 	{ link: "#contact-me", text: "Contact Me" },
-	{
-		link:
-			"https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing",
-		text: "My Resume",
-	},
+	{ link: "/resume", text: "My Resume" },
 ];
 
 export default arr;
