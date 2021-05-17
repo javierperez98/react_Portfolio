da7bb6168a528d3faaeee696be0e904fd95b3490
Javier Perez <javier21perez98@gmail.com>
2021-05-17 01:00:22 -0700

    added a resume page

---
+++ src/componets/resume.js
@@ -0,0 +1,15 @@
+import ResumePage from "./images/resume.png";
+
+function Resume() {
+	return (
+		<section className="pt-5">
+			<img
+				className="img-fluid pt-5"
+				src={ResumePage}
+				alt="Resume of Javier"
+			></img>
+		</section>
+	);
+}
+
+export default Resume;
