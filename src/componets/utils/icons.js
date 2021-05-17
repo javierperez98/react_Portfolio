5ec722c1d7240527e0bc75f17f6d7b1b0b44afaa
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:25:22 -0700

    created file to hold data of icons used in about section

---
+++ src/componets/utils/icons.js
@@ -0,0 +1,16 @@
+const arr = [
+	{ icon: "p-1 fab fa-html5 fa-2x text-danger" },
+	{ icon: "p-1 fab fa-css3-alt fa-2x text-primary" },
+	{ icon: "p-1 fab fa-js-square fa-2x text-js" },
+	{ icon: "p-1 fab fa-react fa-2x text-react" },
+	{ icon: "p-1 fab fa-node-js fa-2x text-success" },
+	{ icon: "p-1 fab fa-npm fa-2x text-white" },
+	{ icon: "p-1 fab fa-bootstrap fa-2x text-bootstrap" },
+	{ icon: "p-1 fab fa-git-square fa-2x text-git" },
+	{ icon: "p-1 fab fa-gitlab fa-2x text-gitlab" },
+	{ icon: "p-1 fab fa-github fa-2x text-github" },
+	{ icon: "p-1 fas fa-database fa-2x text-secondary" },
+	{ icon: "p-1 fas fa-server fa-2x text-white" },
+];
+
+export default arr;
