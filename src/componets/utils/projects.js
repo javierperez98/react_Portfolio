6817891e28cc39d86d306ff7fd64326a27f24a23
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:26:22 -0700

    file created to hold data of project cards

---
+++ src/componets/utils/projects.js
@@ -0,0 +1,41 @@
+import CacheApp from "../images/cache-app.png";
+import EmployeeDirectory from "../images/employee-directory.png";
+import FitnessTracker from "../images/fitness-tracker.png";
+import TechBlog from "../images/tech-blog.png";
+import WeatherPage from "../images/weather-page.png";
+import RocketTrades from "../images/rocket-trades.png";
+
+const arr = [
+	{
+		link: "https://cache-app-io.herokuapp.com/",
+		pic: CacheApp,
+		name: "Cache App",
+	},
+	{
+		link: "https://javierperez98.github.io/employee_directory/",
+		pic: EmployeeDirectory,
+		name: "Employee Directory",
+	},
+	{
+		link: "https://fitness-tracker-app-jp.herokuapp.com",
+		pic: FitnessTracker,
+		name: "Fitness Tracker",
+	},
+	{
+		link: "https://tech-blog-jp.herokuapp.com/",
+		pic: TechBlog,
+		name: "TechBlog App",
+	},
+	{
+		link: "https://javierperez98.github.io/weatherPage/",
+		pic: WeatherPage,
+		name: "WeatherPage App",
+	},
+	{
+		link: "https://dancarrillo93.github.io/Rocket-Trades/",
+		pic: RocketTrades,
+		name: "RocketTrades",
+	},
+];
+
+export default arr;
