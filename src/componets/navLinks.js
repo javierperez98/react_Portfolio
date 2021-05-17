5197889e206eee00f0c22b4ecb0fdcda9599dcc4
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:17:40 -0700

    moved arr of links to a separte file

--- src/componets/navLinks.js
+++ src/componets/navLinks.js
@@ -1,14 +1,6 @@
+import arr from "./utils/navLinks";
+
 function NavLinks() {
-	const arr = [
-		{ link: "#about-me", text: "About Me" },
-		{ link: "#projects", text: "My Projects" },
-		{ link: "#contact-me", text: "Contact Me" },
-		{
-			link:
-				"https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing",
-			text: "My Resume",
-		},
-	];
 	let i = 0;
 	return (
 		<div className="navbar-nav">
