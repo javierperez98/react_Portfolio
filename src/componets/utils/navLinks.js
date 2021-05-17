dcf2569fda1a9a9f534a61f3f8d674548a395a0f
Javier Perez <javier21perez98@gmail.com>
2021-05-16 23:25:56 -0700

    file to hold data of nav links

---
+++ src/componets/utils/navLinks.js
@@ -0,0 +1,12 @@
+const arr = [
+	{ link: "#about-me", text: "About Me" },
+	{ link: "#projects", text: "My Projects" },
+	{ link: "#contact-me", text: "Contact Me" },
+	{
+		link:
+			"https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing",
+		text: "My Resume",
+	},
+];
+
+export default arr;
