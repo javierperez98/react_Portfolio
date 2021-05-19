4cc39510d38d757aa442bc577b50b2b0f21a18fd
Javier Perez <javier21perez98@gmail.com>
2021-05-18 19:02:29 -0700

    updated links to open to a new tab

--- src/componets/contact.js
+++ src/componets/contact.js
@@ -1,15 +1,27 @@
 function Contact() {
 	return (
 		<div className="d-flex flex-row justify-content-around">
-			<a className="text-info px-4" href="https://github.com/javierperez98">
+			<a
+				className="text-info px-4"
+				href="https://github.com/javierperez98"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
 				<i className="fab fa-github fa-2x"></i>
 			</a>
-			<a className="text-success px-4" href="mailto:javier21perez98@gmail.com">
+			<a
+				className="text-success px-4"
+				href="mailto:javier21perez98@gmail.com"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
 				<i className="far fa-envelope fa-2x"></i>
 			</a>
 			<a
 				className="text-primary px-4"
 				href="https://www.linkedin.com/in/javier-perez98/"
+				target="_blank"
+				rel="noopener noreferrer"
 			>
 				<i className="fab fa-linkedin-in fa-2x"></i>
 			</a>
