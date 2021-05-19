56694b164a4876a66767316a57071f2dff2aef32
Javier Perez <javier21perez98@gmail.com>
2021-05-18 19:06:35 -0700

    added discription valve for each project card

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -10,31 +10,43 @@ const arr = [
 		link: "https://cache-app-io.herokuapp.com/",
 		pic: CacheApp,
 		name: "Cache App",
+		active: "active",
+		dis: "Budgeting made simplier! Set a budget and track expenses.",
 	},
 	{
 		link: "https://javierperez98.github.io/employee_directory/",
 		pic: EmployeeDirectory,
 		name: "Employee Directory",
+		active: "",
+		dis: "Employee Search app built using React.js!",
 	},
 	{
 		link: "https://fitness-tracker-app-jp.herokuapp.com",
 		pic: FitnessTracker,
 		name: "Fitness Tracker",
+		active: "",
+		dis: "Track and manage your fitness goals!",
 	},
 	{
 		link: "https://tech-blog-jp.herokuapp.com/",
 		pic: TechBlog,
 		name: "TechBlog App",
+		active: "",
+		dis: "Create and post by signing up.",
 	},
 	{
 		link: "https://javierperez98.github.io/weatherPage/",
 		pic: WeatherPage,
 		name: "WeatherPage App",
+		active: "",
+		dis: "Search any city for current and future weather status!",
 	},
 	{
 		link: "https://dancarrillo93.github.io/Rocket-Trades/",
 		pic: RocketTrades,
 		name: "RocketTrades",
+		active: "",
+		dis: "Search Site for the lastest prices on Stocks and Crypto!",
 	},
 ];
 
