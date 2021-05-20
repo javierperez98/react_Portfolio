2aa04a23b51762d0d54d46d6ee55a6cc0a7a2528
Javier Perez <javier21perez98@gmail.com>
2021-05-19 22:45:32 -0700

    added fade effect

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -1,24 +1,29 @@
 import Contacts from "./contact";
 import Title from "./title";
+import Fade from "react-reveal/Fade";
 
 function Footer() {
 	return (
-		<footer className="d-flex flex-column py-5 bg-gray align-items-center">
-			<h2 className="text-danger text-center mb-4">
-				<Title>Contact Me/</Title>
-			</h2>
-			<Contacts />
-			<h5 className="text-white text-center my-5">Javier Perez</h5>
-			<p className="text-secondary">
-				<i className="fa fa-copyright"></i> Copyright 2021
-			</p>
-			<p className="text-secondary text-center">
-				All rights reserved. Powered by{" "}
-				<span className="text-white">
-					React.js<i className="p-1 fab fa-react text-react"></i>
-				</span>
-			</p>
-		</footer>
+		<div className="bg-gray">
+			<Fade bottom cascade delay={300}>
+				<footer className="d-flex flex-column py-5 align-items-center" id="contact">
+					<h2 className="text-danger text-center mb-4">
+						<Title>Contact Me/</Title>
+					</h2>
+					<Contacts />
+					<h5 className="text-white text-center my-5">Javier Perez</h5>
+					<p className="text-secondary">
+						<i className="fa fa-copyright"></i> Copyright 2021
+					</p>
+					<p className="text-secondary text-center">
+						All rights reserved. Powered by{" "}
+						<span className="text-white">
+							React.js<i className="p-1 fab fa-react text-react"></i>
+						</span>
+					</p>
+				</footer>
+			</Fade>
+		</div>
 	);
 }
 
