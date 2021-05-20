fa33e479afe060c781da9d900ace7527fb4175cd
Javier Perez <javier21perez98@gmail.com>
2021-05-19 22:47:41 -0700

    added fade effect and removed router links

--- src/componets/navLinks.js
+++ src/componets/navLinks.js
@@ -1,24 +1,26 @@
 import arr from "../utils/navLinks";
-import { NavLink } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 function NavLinks() {
 	let i = 0;
 	return (
-		<div className="navbar-nav">
-			{arr.map((arr) => (
-				<NavLink key={(i += 1)} className="nav-link text-white" to={arr.link}>
-					{arr.text}
-				</NavLink>
-			))}
-			<a
-				className="nav-link text-white"
-				href="https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				My Resume
-			</a>
-		</div>
+		<Fade top cascade>
+			<div className="navbar-nav">
+				{arr.map((arr) => (
+					<a key={(i += 1)} className="nav-link text-white" href={arr.link}>
+						{arr.text}
+					</a>
+				))}
+				<a
+					className="nav-link text-white"
+					href="https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					My Resume
+				</a>
+			</div>
+		</Fade>
 	);
 }
 
