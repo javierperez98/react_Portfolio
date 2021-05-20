175d5e185a9f153164474e0574cd5f4fa1cf8ac9
Javier Perez <javier21perez98@gmail.com>
2021-05-19 22:45:57 -0700

    added reveal effects

--- src/componets/home.js
+++ src/componets/home.js
@@ -1,20 +1,28 @@
+import Fade from "react-reveal/Fade";
+import Flash from "react-reveal/Flash";
+
 function Home() {
 	return (
-		<header
-			className="vh-100 bg-custom flex-column d-flex justify-content-center"
-			id="home"
-		>
-			<h1 className="text-warning text-center my-3 mx-auto">
-				<i className="fas fa-chevron-left"></i>Hello!/
-				<i className="fas fa-chevron-right"></i>
-			</h1>
-			<h2 className="text-primary text-center my-3 mx-auto">
-				<i className="fas fa-code-branch"></i>I'm Full-Stack Web-Developer,
-			</h2>
-			<h2 className="text-danger text-center my-3 mx-auto">
-				<i className="fas fa-terminal"></i>Javier Perez
-			</h2>
-		</header>
+		<Fade bottom cascade delay={500}>
+			<header
+				className="vh-100 bg-custom flex-column d-flex justify-content-center"
+				id="home"
+			>
+				<h1 className="text-warning text-center my-3 mx-auto">
+					<i className="fas fa-chevron-left"></i>Hello!/
+					<i className="fas fa-chevron-right"></i>
+				</h1>
+				<h2 className="text-primary text-center my-3 mx-auto">
+					<i className="fas fa-code-branch"></i>I'm Full-Stack Web-Developer,
+				</h2>
+				<h2 className="text-danger text-center my-3 mx-auto">
+					<Flash forever duration={2500}>
+						<i className="fas fa-terminal"></i>
+					</Flash>
+					Javier Perez
+				</h2>
+			</header>
+		</Fade>
 	);
 }
 
