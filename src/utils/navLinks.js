98f9e4d983aa3b46c1c24bb83cdf982c987a9d98
Javier Perez <javier21perez98@gmail.com>
2021-05-19 22:48:46 -0700

    added link to contact me

--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -1,6 +1,7 @@
 const arr = [
-	{ link: "/about", text: "About Me" },
-	{ link: "/projects", text: "My Projects" },
+	{ link: "#about", text: "About Me" },
+	{ link: "#projects", text: "My Projects" },
+	{ link: "#contact", text: "Contact Me" },
 ];
 
 export default arr;
