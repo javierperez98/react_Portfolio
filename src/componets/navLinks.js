3044d4215d5a287369f24e5faa7f7a0b6ed9740e
Javier Perez <javier21perez98@gmail.com>
2021-05-26 21:04:50 -0700

    moved resume link into map array

--- src/componets/navLinks.js
+++ src/componets/navLinks.js
@@ -7,18 +7,16 @@ function NavLinks() {
 		<Fade top cascade>
 			<div className="navbar-nav">
 				{arr.map((arr) => (
-					<a key={(i += 1)} className="nav-link text-white" href={arr.link}>
+					<a
+						key={(i += 1)}
+						className="nav-link text-white"
+						target={arr.target}
+						rel={arr.rel}
+						href={arr.link}
+					>
 						{arr.text}
 					</a>
 				))}
-				<a
-					className="nav-link text-white"
-					href="https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					My Resume
-				</a>
 			</div>
 		</Fade>
 	);
