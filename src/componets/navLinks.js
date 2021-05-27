ede5d2945d0135be1d6e0de597096caabcc0d185
Javier Perez <javier21perez98@gmail.com>
2021-05-26 21:35:10 -0700

    added react scroll to links

--- src/componets/navLinks.js
+++ src/componets/navLinks.js
@@ -1,21 +1,25 @@
 import arr from "../utils/navLinks";
 import Fade from "react-reveal/Fade";
+import { Link } from "react-scroll";
 
 function NavLinks() {
 	let i = 0;
 	return (
-		<Fade top cascade>
+		<Fade top>
 			<div className="navbar-nav">
 				{arr.map((arr) => (
-					<a
+					<Link
+						style={{ cursor: "pointer" }}
+						spy={true}
+						smooth={true}
 						key={(i += 1)}
 						className="nav-link text-white"
 						target={arr.target}
 						rel={arr.rel}
-						href={arr.link}
+						to={arr.link}
 					>
 						{arr.text}
-					</a>
+					</Link>
 				))}
 			</div>
 		</Fade>
