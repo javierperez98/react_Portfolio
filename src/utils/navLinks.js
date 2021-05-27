e4f9a8c08fbf1a6e6a92030f89b4ff342897ec0f
Javier Perez <javier21perez98@gmail.com>
2021-05-26 21:04:21 -0700

    updated links array

--- src/utils/navLinks.js
+++ src/utils/navLinks.js
@@ -1,7 +1,14 @@
 const arr = [
-	{ link: "#about", text: "About Me" },
-	{ link: "#projects", text: "My Projects" },
-	{ link: "#contact", text: "Contact Me" },
+	{ target: "", rel: "", link: "/home/#about", text: "About Me" },
+	{ target: "", rel: "", link: "/home/#projects", text: "My Projects" },
+	{ target: "", rel: "", link: "/home/#contact", text: "Contact Me" },
+	{
+		target: "_blank",
+		rel: "noopener noreferrer",
+		link:
+			"https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing",
+		text: "My Resume",
+	},
 ];
 
 export default arr;
