ab2cfcf7ca596c101b2caa0c6232cf516d2ea0a3
Javier Perez <javier21perez98@gmail.com>
2021-05-30 11:28:33 -0700

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import Navbar from "./componets/navbar";
-import Home from "./componets/home";
-import Main from "./componets/main";
-import Footer from "./componets/footer";
-import Projects from "./componets/projects";
-import About from "./componets/about";
+import Portfolio from "./componets/index";
 
 function App() {
-	return (
-		<>
-			<Navbar />
-			<Home />
-			<Main>
-				<About />
-				<Projects />
-			</Main>
-			<Footer />
-		</>
-	);
+	return <Portfolio />;
 }
 
 export default App;
