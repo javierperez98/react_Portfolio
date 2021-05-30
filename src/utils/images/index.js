31de9addc1d5d7d313c857e97e7466a7d7a5c3e2
Javier Perez <javier21perez98@gmail.com>
2021-05-30 11:35:43 -0700

    updated images and imports

---
+++ src/utils/images/index.js
@@ -0,0 +1,17 @@
+import cacheApp from "./pics/cache-app.png";
+import employeeDirectory from "./pics/employee-directory.png";
+import fitnessTracker from "./pics/fitness-tracker.png";
+import techBlog from "./pics/tech-blog.png";
+import weatherPage from "./pics/weather-page.png";
+import self from "./pics/self.png";
+
+const pics = {
+	CacheApp: cacheApp,
+	EmployeeDirectory: employeeDirectory,
+	FitnessTracker: fitnessTracker,
+	TechBlog: techBlog,
+	WeatherPage: weatherPage,
+	Self: self,
+};
+
+export default pics;
