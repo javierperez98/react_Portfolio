5a7495ed887d4a5983f0e68f36e13a1580b26383
Javier Perez <javier21perez98@gmail.com>
2021-05-30 11:29:02 -0700

    removed webvir

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css";
+import "./styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-reportWebVitals();
