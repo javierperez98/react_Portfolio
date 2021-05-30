ab24028544218ccb00df832875475ccfecc51963
Javier Perez <javier21perez98@gmail.com>
2021-05-30 16:38:03 -0700

    ran purfiycss on style.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css";
+import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
