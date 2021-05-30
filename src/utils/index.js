c679ada21bc5d30308422b20fc8d301aab9373b9
Javier Perez <javier21perez98@gmail.com>
2021-05-30 11:34:30 -0700

    created main file for user info

---
+++ src/utils/index.js
@@ -0,0 +1,56 @@
+import pics from "./images/index";
+
+const user = {
+	name: "Javier Perez",
+	pic: pics.Self,
+	title: "Full-Stack Web-Developer",
+	email: "javier21perez98@gmail.com",
+	github: {
+		user: "javierperez98",
+		link: "https://github.com/javierperez98",
+	},
+	linkedin: {
+		user: "javierperez98",
+		link: "https://www.linkedin.com/in/javier-perez98",
+	},
+	resume:
+		"https://docs.google.com/document/d/117FolzifuTavc4wU3poC1Vd4FT247TOQrqZZIYKNWm4/edit?usp=sharing",
+	bio: "My name is Javier and I'm from West Palm Beach, Florida and I live in San Diego, California. I'm a US Navy active duty military verteran. I love Technology and computers. Web Development is my passion and I want to make a career out of it. I always strive to be the best at what I do. I enjoy working on projects and apps and always aim for the best.",
+	pro1: {
+		link: "https://cache-app-io.herokuapp.com/",
+		repo: "https://github.com/UnDuhDuhSea/CacheApp",
+		pic: pics.CacheApp,
+		title: "Cache App",
+		dis: "Budgeting made simplier, Sign up now!",
+	},
+	pro2: {
+		link: "https://javierperez98.github.io/employee_directory/",
+		repo: "https://github.com/javierperez98/employee_directory",
+		pic: pics.EmployeeDirectory,
+		title: "Employee Directory",
+		dis: "Directory to search and sort built with React.js!",
+	},
+	pro3: {
+		link: "https://fitness-tracker-app-jp.herokuapp.com",
+		repo: "https://github.com/javierperez98/fitness_app",
+		pic: pics.FitnessTracker,
+		title: "Fitness Tracker",
+		dis: "Track and manage your fitness goals!",
+	},
+	pro4: {
+		link: "https://tech-blog-jp.herokuapp.com/",
+		repo: "https://github.com/javierperez98/Tech_Blog",
+		pic: pics.TechBlog,
+		title: "TechBlog App",
+		dis: "Sign up and post to the Blog site!",
+	},
+	pro5: {
+		link: "https://javierperez98.github.io/weatherPage/",
+		repo: "https://github.com/javierperez98/weatherPage",
+		pic: pics.WeatherPage,
+		title: "WeatherPage App",
+		dis: "Weather forecast App for any city you search!",
+	},
+};
+
+export default user;
