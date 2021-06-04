2976dac2bddbb3f4a3fef20e5b4eb97808417a16
Javier Perez <javier21perez98@gmail.com>
2021-06-03 23:39:43 -0700

    added icons for languages used for each project

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,35 +21,72 @@ const user = {
 		repo: "https://github.com/UnDuhDuhSea/CacheApp",
 		pic: pics.CacheApp,
 		title: "Cache App",
-		dis: "Budgeting made simplier! Track your expenses and budget. Input how much you have and how much you spent to get on an easy to use webpage!",
+		dis: "Budgeting made simplier! Track your expenses and budget.",
+		lang: (
+			<>
+				<i className="fas fa-database text-secondary p-1 fa-2x" />
+				<svg width="30" height="18" fill="#16BDCA">
+					<path d="M15 0c-4 0-6.5 2-7.5 6 1.5-2 3.25-2.75 5.25-2.25 1.141.285 1.957 1.113 2.86 2.03C17.08 7.271 18.782 9 22.5 9c4 0 6.5-2 7.5-6-1.5 2-3.25 2.75-5.25 2.25-1.141-.285-1.957-1.113-2.86-2.03C20.42 1.728 18.718 0 15 0zM7.5 9C3.5 9 1 11 0 15c1.5-2 3.25-2.75 5.25-2.25 1.141.285 1.957 1.113 2.86 2.03C9.58 16.271 11.282 18 15 18c4 0 6.5-2 7.5-6-1.5 2-3.25 2.75-5.25 2.25-1.141-.285-1.957-1.113-2.86-2.03C12.92 10.729 11.218 9 7.5 9z"></path>
+				</svg>
+				<i className="fab fa-js-square text-js p-1 fa-2x" />
+			</>
+		),
 	},
 	pro2: {
 		link: "https://javierperez98.github.io/employee_directory/",
 		repo: "https://github.com/javierperez98/employee_directory",
 		pic: pics.EmployeeDirectory,
 		title: "Employee Directory",
-		dis: "Directory to search and sort a list of employees displaying their name, email, phone, picture and DOB built with React.js!",
+		dis: "Directory to search and sort a list of employees displaying their name, email, phone, picture and DOB.",
+		lang: (
+			<>
+				<i className="fab fa-react text-react p-1 fa-2x" />
+				<i className="fab fa-bootstrap text-bootstrap p-1 fa-2x" />
+				<i className="fab fa-js-square text-js p-1 fa-2x" />
+			</>
+		),
 	},
 	pro3: {
 		link: "https://fitness-tracker-app-jp.herokuapp.com",
 		repo: "https://github.com/javierperez98/fitness_app",
 		pic: pics.FitnessTracker,
 		title: "Fitness Tracker",
-		dis: "Track and manage your fitness goals! Input your workouts and excersies to get see your total time spent, total weight and total distance!",
+		dis: "Track and manage your fitness goals! Input your workouts and excersies and track your totals!",
+		lang: (
+			<>
+				<i className="fab fa-envira text-success p-1 fa-2x" />
+				<i className="fab fa-node-js text-success p-1 fa-2x" />
+				<i className="fab fa-js-square text-js p-1 fa-2x" />
+			</>
+		),
 	},
 	pro4: {
 		link: "https://tech-blog-jp.herokuapp.com/",
 		repo: "https://github.com/javierperez98/Tech_Blog",
 		pic: pics.TechBlog,
 		title: "TechBlog App",
-		dis: "Sign up and post to the Blog site! Create an account where you can create posts and view other accounts that post aswell.",
+		dis: "Sign up and post to the Blog site! You can create and view posts.",
+		lang: (
+			<>
+				<i className="fas fa-database text-secondary p-1 fa-2x" />
+				<i className="fab fa-node-js text-success p-1 fa-2x" />
+				<i className="fab fa-js-square text-js p-1 fa-2x" />
+			</>
+		),
 	},
 	pro5: {
 		link: "https://javierperez98.github.io/weatherPage/",
 		repo: "https://github.com/javierperez98/weatherPage",
 		pic: pics.WeatherPage,
 		title: "WeatherPage App",
-		dis: "Weather forecast App to get a current and 5 day forecast for any city you search!",
+		dis: "Weather forecast App that lets you search any city!",
+		lang: (
+			<>
+				<i className="fab fa-html5 text-danger p-1 fa-2x" />
+				<i className="fab fa-bootstrap text-bootstrap p-1 fa-2x" />
+				<i className="fab fa-js-square text-js p-1 fa-2x" />
+			</>
+		),
 	},
 };
 
