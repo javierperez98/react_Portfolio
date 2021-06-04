dda1a6b741afe9e4d09a7c3eb63e585f74f9b290
Javier Perez <javier21perez98@gmail.com>
2021-06-03 23:38:00 -0700

    added props for icons

--- src/componets/projects/cards.js
+++ src/componets/projects/cards.js
@@ -7,17 +7,19 @@ function Card({
 	link1,
 	repo1,
 	dis1,
+	langs1,
 	pic2,
 	alt2,
 	title2,
-	dis2,
 	link2,
 	repo2,
+	dis2,
+	langs2,
 }) {
 	return (
 		<>
 			<Fade right delay={500}>
-				<div className="row gx-0 mb-5 mb-lg-0 justify-content-center">
+				<div className="row gx-0 mb-lg-5 mb-lg-0 justify-content-center">
 					<div className="col-lg-6">
 						<img className="img-fluid custom-border" src={pic1} alt={alt1} />
 					</div>
@@ -27,7 +29,7 @@ function Card({
 								<div className="project-text w-100 my-auto text-center text-lg-left">
 									<h4 className="text-white">{title1}</h4>
 									<p className="mb-0 text-white-50">{dis1}</p>
-									<hr className="d-none d-lg-block mb-0 ms-0" />
+									<p className="my-3">{langs1}</p>
 									<div className="row d-flex justify-content-evenly">
 										<a
 											href={link1}
@@ -63,7 +65,7 @@ function Card({
 								<div className="project-text w-100 my-auto text-center text-lg-right">
 									<h4 className="text-white">{title2}</h4>
 									<p className="mb-0 text-white-50">{dis2}</p>
-									<hr className="d-none d-lg-block mb-0 me-0" />
+									<p className="my-3">{langs2}</p>
 									<div className="row d-flex justify-content-evenly">
 										<a
 											href={link2}
