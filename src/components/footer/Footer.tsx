bd556af83782d8d5a9765702c250318f23641c18
javierperez98 <javier21perez98@gmail.com>
2022-08-20 20:14:39 -0400

    footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,43 @@
+import "./footer.scss";
+import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <a href="#root" className="footer-logo">
+        Javier Perez
+      </a>
+      <ul className="footer-links">
+        <li>
+          <a href="#root">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer-socials">
+        <a href="https://www.linkedin.com/in/javier-perez98/">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/javierperez98">
+          <BsGithub />
+        </a>
+        <a href="https://www.instagram.com/stavi_javi">
+          <BsInstagram />
+        </a>
+      </div>
+      <div className="footer-cr">
+        <small>&copy; Javier Perez. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
